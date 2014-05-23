7db54c5548d50935c372a63c9c37b2cd6085f876
matruskan <matruskan@gmail.com>
2014-05-23 02:12:47 -0300

    Criação de gráfico de exemplo + CSS simples
    O servlet e o javascript foram consertados para que um gráfico de
    exemplo funcionasse.
    Um CSS simples foi adicionado.

--- src/java/br/org/universa/comparadorsocioculturaldoenem/servlet.java
+++ src/java/br/org/universa/comparadorsocioculturaldoenem/servlet.java
@@ -35,21 +35,22 @@ public class servlet extends HttpServlet {
         try {
             String fator = request.getParameter("fator");
             if(fator!=null){
-                String s = "{labels : [\"DF\",\"GO\"],"+
-	"datasets : ["+
+                String s =
+"{ \"labels\" : [\"DF\",\"GO\",\"SP\",\"MG\",\"RS\",\"SC\"],"+
+	"\"datasets\" : ["+
 		"{"+
-			"fillColor : \"rgba(220,220,220,0.5)\","+
-			"strokeColor : \"rgba(220,220,220,1)\","+
-			"pointColor : \"rgba(220,220,220,1)\","+
-			"pointStrokeColor : \"#fff\","+
-			"data : [65,59,90,81,56,55,40]"+
+			"\"fillColor\" : \"rgba(220,220,220,0.5)\","+
+			"\"strokeColor\" : \"rgba(220,220,220,1)\","+
+			"\"pointColor\" : \"rgba(220,220,220,1)\","+
+			"\"pointStrokeColor\" : \"#fff\","+
+			"\"data\" : [65,59,90,81,56,55,40]"+
 		"},"+
 		"{"+
-                	"fillColor : \"rgba(151,187,205,0.5)\","+
-			"strokeColor : \"rgba(151,187,205,1)\","+
-			"pointColor : \"rgba(151,187,205,1)\","+
-			"pointStrokeColor : \"#fff\","+
-			"data : [28,48,40,19,96,27,100]"+
+                	"\"fillColor\" : \"rgba(151,187,205,0.5)\","+
+			"\"strokeColor\" : \"rgba(151,187,205,1)\","+
+			"\"pointColor\" : \"rgba(151,187,205,1)\","+
+			"\"pointStrokeColor\" : \"#fff\","+
+			"\"data\" : [28,48,40,19,96,27,100]"+
 		"}"+
 	"]"+
 "}";
